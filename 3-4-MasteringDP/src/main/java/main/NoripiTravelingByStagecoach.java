0d90b1aa31433e5f1476a104d78c482ee91a8d67
noriyuki.ishida <noriyuki106@gmail.com>
2015-12-03 21:16:58 +0900

    noripi's answer

---
+++ 3-4-MasteringDP/src/main/java/main/NoripiTravelingByStagecoach.java
@@ -0,0 +1,14 @@
+package main;
+
+import java.util.Arrays;
+
+public class NoripiTravelingByStagecoach implements TravelingByStagecoachInterface {
+
+  @Override
+  public double solve(int n, int m, int a, int b, int[] t) {
+    System.out.println(Arrays.toString(t)); 
+    
+    return 0;
+  }
+
+}
