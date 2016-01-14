89fa2fa86d903cf1e6743eacfc2006bbccc28c0a
noriyuki.ishida <noriyuki106@gmail.com>
2016-01-14 20:55:21 +0900

    fix test

--- 3-5-NetworkFlow/src/test/java/NetworkFlowTest.java
+++ 3-5-NetworkFlow/src/test/java/NetworkFlowTest.java
@@ -1,54 +1,49 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.security.spec.ECGenParameterSpec;
-import java.util.HashMap;
-
 public class NetworkFlowTest {
 
-    // replace by your implementation class here
-    NetworkFlow yourImplementation = new NetworkFlowExample();
+  // replace by your implementation class here
+  NetworkFlow yourImplementation = new NetworkFlowExample();
 
-    @Test
-    public void networkFlowTest1() {
-        int[][] graph = new int[5][5];
-        graph[0][0] = 0;
-        graph[0][1] = 10;
-        graph[0][2] = 2;
-        graph[1][2] = 6;
-        graph[1][3] = 6;
-        graph[2][4] = 5;
-        graph[3][2] = 3;
-        graph[3][4] = 8;
-        graph[4][4] = 0;
+  @Test
+  public void networkFlowTest1() {
+    int[][] graph = new int[4][5];
+    graph[0][1] = 10;
+    graph[0][2] = 2;
+    graph[1][2] = 6;
+    graph[1][3] = 6;
+    graph[2][4] = 5;
+    graph[3][2] = 3;
+    graph[3][4] = 8;
 
-        NetworkFlowSolverWrapper solver = new NetworkFlowSolverWrapper(0, 4, graph);
-        timeMeasuredAssertEquals(11, solver);
+    NetworkFlowSolverWrapper solver = new NetworkFlowSolverWrapper(0, 4, graph);
+    timeMeasuredAssertEquals(11, solver);
+  }
+
+
+  private void timeMeasuredAssertEquals(int expected, NetworkFlowSolverWrapper solver) {
+    long start = System.nanoTime();
+    Assert.assertEquals(expected, solver.solve());
+    long end = System.nanoTime();
+
+    System.out
+        .println(String.format("Finished execution in: %f millis.", (end - start) / 1000000f));
+  }
+
+  private class NetworkFlowSolverWrapper {
+    int S;
+    int T;
+    int[][] G;
+
+    NetworkFlowSolverWrapper(int s, int t, int[][] g) {
+      this.S = s;
+      this.T = t;
+      this.G = g;
     }
 
-
-    private void timeMeasuredAssertEquals(int expected, NetworkFlowSolverWrapper solver) {
-        long start = System.nanoTime();
-        Assert.assertEquals(expected, solver.solve());
-        long end = System.nanoTime();
-
-        System.out.println(String.format("Finished execution in: %f millis." , (end - start) / 1000000f ));
+    int solve() {
+      return yourImplementation.getMaxFow(S, T, G);
     }
-
-    private class NetworkFlowSolverWrapper {
-        int S;
-        int T;
-        int[][] G;
-
-        NetworkFlowSolverWrapper(int s, int t, int[][] g) {
-            this.S = s;
-            this.T = t;
-            this.G = g;
-        }
-
-        int solve() {
-            return 11;
-            //return yourImplementation.getMaxFow(S, T, G);
-        }
-    }
+  }
 }
