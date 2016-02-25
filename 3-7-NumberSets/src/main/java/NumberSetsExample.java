5777d13cdb63125a08e406fa2bbae44a3db1d4b6
noriyuki.ishida <noriyuki106@gmail.com>
2016-02-25 20:38:51 +0900

    update example

--- 3-7-NumberSets/src/main/java/NumberSetsExample.java
+++ 3-7-NumberSets/src/main/java/NumberSetsExample.java
@@ -2,15 +2,14 @@
 public class NumberSetsExample implements NumberSetsInterface {
 
   @Override
-  public int solve(int A, int B, int P) {
-    switch (P) {
-      case 5:
-        return 9;
-      case 3:
-        return 7;
-      default:
-        return 0;
+  public int solve(long A, long B, long P) {
+    if (P == 5) {
+      return 9;
     }
+    if (P == 3) {
+      return 7;
+    }
+    return 0;
   }
 
 }
