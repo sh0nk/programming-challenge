da14a9e3d0360eaf0cd3bdb189822437a2696ed3
sh0nk <sh0nk.developer@gmail.com>
2016-03-03 19:14:44 +0900

    4-1-InclusionAndExclusion: Add a module

---
+++ 4-1-InclusionAndExclusion/src/main/java/InclusionAndExclusionExample.java
@@ -0,0 +1,7 @@
+public class InclusionAndExclusionExample implements InclusionAndExclusionInterface {
+
+    @Override
+    public int solve(int n, int m, int[] a) {
+        return 0;
+    }
+}
---
+++ 4-1-InclusionAndExclusion/src/main/java/InclusionAndExclusionInterface.java
@@ -0,0 +1,14 @@
+/**
+ * <pre>
+ * 1以上n以下の整数で、a1, a2,...,amのうち、少なくとも1つで割り切れるものの個数を計算しなさい。
+ *
+ * <p> 制約
+ * Small
+ * ・1 ≦ n ≦ 10^9
+ * ・2 ≦ m ≦ 15
+ * </pre>
+ * 
+ */
+public interface InclusionAndExclusionInterface {
+    public int solve(int n, int m, int[] a);
+}
