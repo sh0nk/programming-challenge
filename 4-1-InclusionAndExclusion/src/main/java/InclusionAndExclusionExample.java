2d6585a4c34756faa09073560480b9ddd2e7a7bf
sh0nk <sh0nk.developer@gmail.com>
2016-03-03 20:55:07 +0900

    Add log4j2 configurations

--- 4-1-InclusionAndExclusion/src/main/java/InclusionAndExclusionExample.java
+++ 4-1-InclusionAndExclusion/src/main/java/InclusionAndExclusionExample.java
@@ -1,7 +1,12 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class InclusionAndExclusionExample implements InclusionAndExclusionInterface {
+    private static Logger LOGGER = LogManager.getLogger(InclusionAndExclusionExample.class);
 
     @Override
     public int solve(int n, int m, int[] a) {
+        LOGGER.info("{} is called with params, <{}> <{}> <{}>", this.getClass(), n, m, a);
         return 0;
     }
 }
