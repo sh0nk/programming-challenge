484142c83a4e2044c72017caf922a8ee5553f1ee
大西信寛 <onishinobuhiro@oonishinobuhiro-no-MacBook-Pro.local>
2016-03-16 08:09:09 +0900

    4-3 graph master

---
+++ 4-3-GraphMaster/src/test/java/PopularCowsTest.java
@@ -0,0 +1,33 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PopularCowsTest {
+  private long startTime;
+
+  private PopularCowsInterface yourImplementation = new PopularCowsExample();
+
+  @Before
+  public void setUp() {
+    this.startTime = System.nanoTime();
+  }
+
+  @After
+  public void tearDown() {
+    double timeElapsed = (System.nanoTime() - this.startTime) / 1_000_000f;
+    System.out.println(
+        String.format("Finished execution in: %f millis.", timeElapsed));
+  }
+
+  @Test
+  public void test1() {
+    int N = 3;
+    int M = 3;
+    int[] A = {1, 2, 2};
+    int[] B = {2, 1, 3};
+    assertEquals(1, this.yourImplementation.solve(N, M, A, B));
+  }
+
+}
