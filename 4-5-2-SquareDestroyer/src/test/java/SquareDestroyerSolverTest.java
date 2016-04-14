3774f8a5e904c628cb0bdc15bb45b683ff3e10d8
noriyuki.ishida <noriyuki106@gmail.com>
2016-04-14 22:58:38 +0900

    add test case

--- 4-5-2-SquareDestroyer/src/test/java/SquareDestroyerSolverTest.java
+++ 4-5-2-SquareDestroyer/src/test/java/SquareDestroyerSolverTest.java
@@ -1,9 +1,8 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
 
 public class SquareDestroyerSolverTest {
   private long startTime;
@@ -22,36 +21,44 @@ public class SquareDestroyerSolverTest {
         String.format("Finished execution in: %f millis.", timeElapsed));
   }
 
-    @Test
-    public void test1() {
-        int n = 2;
-        int[] excludes = new int[] {};
+  @Test
+  public void test1() {
+    int n = 2;
+    int[] excludes = new int[] {};
 
-        assertEquals(3, this.yourImplementation.solve(n, excludes));
-    }
+    assertEquals(3, this.yourImplementation.solve(n, excludes));
+  }
 
-    @Test
-    public void test2() {
-        int n = 3;
-        int[] excludes = new int[] {12, 17, 23};
+  @Test
+  public void test2() {
+    int n = 3;
+    int[] excludes = new int[] {12, 17, 23};
 
-        assertEquals(3, this.yourImplementation.solve(n, excludes));
-    }
+    assertEquals(3, this.yourImplementation.solve(n, excludes));
+  }
 
-    @Test
-    public void test3() {
-        int n = 2;
-        int[] excludes = new int[] {5, 7, 10, 12};
+  @Test
+  public void test3() {
+    int n = 2;
+    int[] excludes = new int[] {5, 7, 10, 12};
 
-        assertEquals(1, this.yourImplementation.solve(n, excludes));
-    }
+    assertEquals(1, this.yourImplementation.solve(n, excludes));
+  }
 
-    @Test
-    public void test4() {
-        int n = 3;
-        int[] excludes = new int[] {14, 17, 20, 21, 23, 24};
+  @Test
+  public void test4() {
+    int n = 3;
+    int[] excludes = new int[] {14, 17, 20, 21, 23, 24};
 
-        assertEquals(4, this.yourImplementation.solve(n, excludes));
-    }
+    assertEquals(4, this.yourImplementation.solve(n, excludes));
+  }
+
+  @Test
+  public void test5() {
+    int n = 4;
+    int[] excludes = new int[] {14, 17, 20, 21, 23, 24};
+
+    assertEquals(5, this.yourImplementation.solve(n, excludes));
+  }
 
 }
