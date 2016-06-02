d7aed64437c0e07c51a6757df69653f67966cb5f
sh0nk <sh0nk.developer@gmail.com>
2016-06-02 20:43:51 +0900

    2-4-2: Fix test case

--- 2-4-2-FoodChain/src/test/java/Test.java
+++ 2-4-2-FoodChain/src/test/java/Test.java
@@ -8,8 +8,8 @@ public class Test {
     public void test1() {
         final int ANSWER = 3;
 
-        int N = 4;
-        int K = 10;
+        int N = 100;
+        int K = 7;
         int[] type = new int[] { 1, 2, 2, 2, 1, 2, 1 };
         int[] x = new int[] { 101, 1, 2, 3, 1, 3, 5 };
         int[] y = new int[] { 1, 2, 3, 3, 3, 1, 5 };
