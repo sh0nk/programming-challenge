8e4e3b3d4380e1e1d2cd35de8be23fa855b030b7
sh0nk <sh0nk.developer@gmail.com>
2016-06-02 20:43:00 +0900

    2-4-2 Problem: Food Chain

---
+++ 2-4-2-FoodChain/src/main/java/Interface.java
@@ -0,0 +1,5 @@
+
+public interface Interface {
+    public int execute(int N, int K, int[] type, int[] x, int[] y);
+
+}
---
+++ 2-4-2-FoodChain/src/test/java/Test.java
@@ -0,0 +1,25 @@
+import org.junit.Assert;
+
+public class Test {
+
+    private Interface yourImplementation = null;
+
+    @org.junit.Test
+    public void test1() {
+        final int ANSWER = 3;
+
+        int N = 4;
+        int K = 10;
+        int[] type = new int[] { 1, 2, 2, 2, 1, 2, 1 };
+        int[] x = new int[] { 101, 1, 2, 3, 1, 3, 5 };
+        int[] y = new int[] { 1, 2, 3, 3, 3, 1, 5 };
+
+        long start = System.nanoTime();
+        Assert.assertEquals(ANSWER, yourImplementation.execute(N, K, type, x, y));
+        long end = System.nanoTime();
+
+        System.out.println(String.format("Finished execution in: %f millis.", (end - start) / 1000000f));
+    }
+
+
+}
