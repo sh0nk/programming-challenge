b81f2b4dca90d9536571c24fb17ed11d1a2bb3d5
Nobby <nobuhiro.onishi@rakuten.com>
2016-06-16 23:24:40 +0900

    sorry, fix me

--- 4-6-ClosestPair/src/main/java/ClosestPairSolverExample.java
+++ 4-6-ClosestPair/src/main/java/ClosestPairSolverExample.java
@@ -2,36 +2,73 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.awt.geom.Point2D;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 public class ClosestPairSolverExample implements IClosestPairSolver {
     private static Logger logger = LogManager.getLogger(ClosestPairSolverExample.class);
 
-    int N;
-    Point2D.Double A[] = new Point2D.Double[10000];
-
-    //TODO 途中なので完成させる
+    //TODO sorry , this is not correct.
     public double solve(int n, double[] X, double[] Y) {
-        N = n;
-        // xを昇順ソートしておく
-        Arrays.sort(X);
-
+        Point2D.Double A[] = new Point2D.Double[n];
         for (int i = 0; i < X.length; i++) {
             A[i] = new Point2D.Double(X[i], Y[i]);
         }
 
-        if (n <= 1) return Double.POSITIVE_INFINITY;
-        int m = n / 2;
-        double x = A[m].getX();
+        // xを昇順ソートしておく
+        Arrays.sort(A, new Comparator<Point2D.Double>() {
+            @Override
+            public int compare(Point2D.Double o1, Point2D.Double o2) {
+                return (int)(o1.getX() - o2.getX());
+            }
+        });
+        Arrays.stream(A).forEach(a -> {
+            logger.info("x, y : ({}, {})", a.getX(), a.getY());
+        });
 
-
-
-
-
-        return 36.2215;
+        double ans = closest_pair(A, n);
+        return ans;
     }
 
-    private boolean compare_y(Point2D.Double a, Point2D.Double b) {
-        return a.getX() < b.getY();
+    /**
+     * @param a x座標の昇順になっている配列
+     * @param n
+     * @return
+     */
+    private double closest_pair(Point2D.Double[] a, int n) {
+        //logger.info("n count : {}", n);
+        if (n <= 1) return Double.POSITIVE_INFINITY;
+        int m = n / 2;
+        double x = a[m].getX();
+        Point2D.Double[] a1 = Arrays.copyOfRange(a, 0, m);
+        Point2D.Double[] a2 = Arrays.copyOfRange(a, m, n - m);
+
+        double d = Math.min(closest_pair(a1, m), closest_pair(a2, n - m));
+
+        // yの昇順にする
+        Arrays.sort(a, new Comparator<Point2D.Double>() {
+            @Override
+            public int compare(Point2D.Double o1, Point2D.Double o2) {
+                return (int)(o1.getY() - o2.getY());
+            }
+        });
+
+        // 距離d未満の頂点を取得していく
+        List<Point2D.Double> b = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (Math.abs(a[i].getX() - x) >= d) continue;
+
+            for (int j = 0; j < b.size(); j++) {
+                double dx = a[i].getX() - b.get(b.size() - j - 1).getX();
+                double dy = a[i].getY() - b.get(b.size() - j - 1).getY();
+                if (dy >= d) break;
+
+                d = Math.min(d, Math.sqrt(dx * dx + dy * dy));
+            }
+            b.add(a[i]);
+        }
+        return d;
     }
 }
